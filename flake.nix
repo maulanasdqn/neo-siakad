c05077e4c0530f0556039c226e549d56477b9a1d
Maulana Sodiqin <maulanasdqn@gmail.com>
2024-02-12 18:18:12 +0700

    chore: update nix flake

--- flake.nix
+++ flake.nix
@@ -1,36 +1,98 @@
 {
-  description = "Sample Nix ts-node build";
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    gitignore = {
-      url = "github:hercules-ci/gitignore.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    dream2nix.url = "github:nix-community/dream2nix";
-  };
-  outputs = { self, nixpkgs, flake-utils, gitignore, dream2nix, ... }:
-    let
-      dream2nixOutputs = dream2nix.lib.makeFlakeOutputs {
-        systems = flake-utils.lib.defaultSystems;
-        config.projectRoot = ./.;
-        source = gitignore.lib.gitignoreSource ./.;
-      };
-      customOutput = flake-utils.lib.eachDefaultSystem (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          app = dream2nixOutputs.packages."${system}".example-node-nix;
-        in with pkgs; {
-          packages.docker = dockerTools.buildImage {
-            name = app.packageName;
-            copyToRoot = pkgs.buildEnv {
-              name = app.packageName;
-              paths = [ app ];
-              pathsToLink = [ "/bin" "/lib" ];
+  description = "Nix NodeJS Version Builder";
+
+  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [ ];
+      systems =
+        [ "aarch64-darwin" "x86_64-darwin" "x86_64-linux" "aarch64-linux" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        packages = {
+          nodejs21-darwin-arm64 = pkgs.stdenv.mkDerivation {
+            name = "nodejs21.6.1-darwin-arm64";
+            src = pkgs.fetchurl {
+              url =
+                "https://nodejs.org/dist/v21.6.1/node-v21.6.1-darwin-arm64.tar.gz";
+              sha256 = "sha256-rY2PxTMO9HeI9QnCrzmMgGC7Way+kUBw0N9oTNLY05s";
             };
-            keepContentsDirlinks = true;
-            config = { Cmd = [ "/bin/ts-node-nix" ]; };
+            installPhase = ''
+              echo "installing nodejs"
+              mkdir -p $out
+              cp -r ./ $out/
+            '';
           };
-        });
-    in nixpkgs.lib.recursiveUpdate dream2nixOutputs customOutput;
+
+          nodejs21-darwin-x64 = pkgs.stdenv.mkDerivation {
+            name = "nodejs21.6.1-darwin-x64";
+            src = pkgs.fetchurl {
+              url =
+                "https://nodejs.org/dist/v21.6.1/node-v21.6.1-darwin-x64.tar.gz";
+              sha256 = "sha256-N+CajPI1LzQNEgTGFUBY2BNi/vTsSIsBl7LONrPwNno=";
+            };
+            installPhase = ''
+              echo "installing nodejs"
+              mkdir -p $out
+              cp -r ./ $out/
+            '';
+          };
+
+          nodejs21-linux-x64 = pkgs.stdenv.mkDerivation {
+            name = "nodejs21.6.1-linux-x64";
+            src = pkgs.fetchurl {
+              url =
+                "https://nodejs.org/dist/v21.6.1/node-v21.6.1-linux-x64.tar.gz";
+              sha256 = "sha256-0cHeRhvhC/2ccOuuRzMPsbSrCpitcwgj+xNA40mT7e4=";
+            };
+            installPhase = ''
+              echo "installing nodejs"
+              mkdir -p $out
+              cp -r ./ $out/
+            '';
+          };
+
+          nodejs21-linux-arm64 = pkgs.stdenv.mkDerivation {
+            name = "nodejs21.6.1-linux-arm64";
+            src = pkgs.fetchurl {
+              url =
+                "https://nodejs.org/dist/v21.6.1/node-v21.6.1-linux-arm64.tar.gz";
+              sha256 = "sha256-KqOH5qV63mY4Se/cT6v3Qxo42XXbmNzHkpOEDmiU0os=";
+            };
+            installPhase = ''
+              echo "installing nodejs"
+              mkdir -p $out
+              cp -r ./ $out/
+            '';
+          };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            (if system == "aarch64-linux" then
+              self'.packages.nodejs21-linux-arm64
+            else if system == "aarch64-darwin" then
+              self'.packages.nodejs21-darwin-arm64
+            else if system == "x86_64-linux" then
+              self'.packages.nodejs21-linux-x64
+            else if system == "x86_64-darwin" then
+              self'.packages.nodejs21-darwin-x64
+            else
+              null)
+            pkgs.bun
+            pkgs.gnused
+            pkgs.docker
+            pkgs.nodePackages.node-gyp-build
+            pkgs.nodePackages."dockerfile-language-server-nodejs"
+            pkgs.nodePackages."typescript"
+            pkgs.nodePackages."typescript-language-server"
+            pkgs.nodePackages."vscode-langservers-extracted"
+            pkgs.nodePackages."@tailwindcss/language-server"
+            pkgs.emmet-ls
+          ];
+
+        };
+      };
+      flake = { };
+    };
 }
